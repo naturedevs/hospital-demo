84c730cf3ebc6b44096b9b152b5cb7267b1a985e
Jay <wmeng0103@gmail.com>
2024-03-08 03:47:43 +0900

    init

--- src/pages/landingpage/Landingpage.js
+++ src/pages/landingpage/Landingpage.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect, useRef} from 'react'
 import {MapContainer, TileLayer} from 'react-leaflet';
 import { Switch,Input  } from "@material-tailwind/react";
 import {DatePicker} from "antd"
@@ -12,42 +12,40 @@ import Dropdown from '../../components/dropdown/Dropdown';
 
 function Landingpage() {
   var center = {lat: localStorage.getItem('lati1'), lng: localStorage.getItem('long1')};
+  
+  useEffect(() => {
+    const element = ref.current;
+    if (element) {
+      element.addEventListener('touchstart', onTouchStart, false);
+
+      return () => element.removeEventListener('touchstart', onTouchStart);
+    }
+ }, []);
   const [zoom, setZoom] = useState(13);
-  const [word, setWord] = React.useState("");
-  const [mapOn, setMapOn] = useState(false);
-  const [y, setY] = useState(190)
-  const [mapY, setMapY] = useState(290)
-  const [startY, setStartY] = useState(null)
-  const [mapMove, setMapMove] = useState(false)
   const people = data.data
-  
-  const onMovebarClick = (e) => {
-    setMapMove(!mapMove)
-  }
-  const onMouseDown = (e) => {
-    setMapOn(true);
-    setStartY(e.clientY);
-    // Attach pointermove and mouseup event listeners
-    document.addEventListener('pointermove', onMouseMove);
-    document.addEventListener('pointerup', onMouseUp);
-   };
-   
-  const onMouseMove = (e) => {
-    if (!mapOn) return; // Ensure mapOn is true to proceed
-    // let change = e.clientY - startY;
-    // setY(y => y - change);
-    // setMapY(mapY => mapY - change);
-  };
-  
-  const onMouseUp = (e) => {
-    setMapOn(false);
-    // Remove event listeners
-    document.removeEventListener('pointermove', onMouseMove);
-    document.removeEventListener('pointerup', onMouseUp);
-  };
-  const onSelect = (e) => {
-      setWord(e);
-  };
+  const [yPosition, setYPosition] = useState(250);
+  const ref = useRef(null);
+ 
+  const onTouchStart = (event) => {
+    event.preventDefault();
+    const startY = event.touches[0].clientY - yPosition;
+
+    const onTouchMove = (event) => {
+      const newY = event.touches[0].clientY - startY;
+      if(yPosition > 150) setYPosition(newY);
+      console.log(yPosition);
+    };
+
+    const onTouchEnd = () => {
+      document.removeEventListener('touchmove', onTouchMove);
+      document.removeEventListener('touchend', onTouchEnd);
+    };
+
+    document.addEventListener('touchmove', onTouchMove, { passive: false });
+    document.addEventListener('touchend', onTouchEnd);
+ };
+ 
+
   function truncateText(text, maxLength) {
     return text.length > maxLength ? text.substring(0, maxLength) + '...' : text;
   }
@@ -57,8 +55,8 @@ function Landingpage() {
       <div className='w-full relative top-0 z-100'>
         <Header />
       </div>
-      <div className = "w-full flex relative sm:relative h-[calc(100vh-90px)] lg:space-x-5  px-4 z-0"  onMouseMove={onMouseMove} onMouseUp={onMouseUp}>
-        <div className={`mapContainerDiv w-full overflow-hidden h-[calc(100vh-500px)] md:h-auto rounded-lg top-0 md:top-auto absolute bottom-0 left-0 md:left-auto right-0 md:right-auto md:relative z-10`}>
+      <div className = "w-full flex relative sm:relative h-[calc(100vh-90px)] lg:space-x-5  px-4 z-0" >
+        <div className={`mapContainerDiv w-full overflow-hidden h-[calc(100vh-120px)] md:h-auto rounded-lg top-0 md:top-auto absolute bottom-0 left-0 md:left-auto right-0 md:right-auto md:relative z-10`}>
           <MapContainer
             center={center}
             zoom={zoom}
@@ -76,12 +74,6 @@ function Landingpage() {
             <div className='flex'>
               <Dropdown />
               <div className='w-[150px]'>
-                {/* <Input 
-                  type='date'
-                  width={'120px'}
-                  defaultValue={12/12/2023}
-                  onTimeUpdate={true}
-                /> */}
                 <DatePicker 
                   size='large'
                 />
@@ -98,8 +90,8 @@ function Landingpage() {
               </p>
             </div>
           </div>
-          <div className={`bg-white bottom-0 md:bottom-auto w-full h-[410px] md:h-full rounded-xl absolute md:static right-0 pl-5 z-20 overflow-y-auto pb-10 pr-3 md:pr-0`}>
-            <div className='w-full cursor-move'onMouseDown={onMouseDown} onClick={onMovebarClick}>
+          <div className={`bg-white bottom-0 md:bottom-auto w-full invisible sm:visible md:h-full rounded-xl absolute md:static right-0 pl-5 z-20 overflow-y-auto pb-10 pr-3 md:pr-0`}>
+            <div className='w-full cursor-move'>
               <div className='w-full h-[2px] bg-gray-800 visible md:hidden'  />
               {/* <img src='/images/logo.png'/> */}
             </div>
@@ -109,12 +101,6 @@ function Landingpage() {
                   <Dropdown />
                 </div>
                 <div className='w-[150px] mt-4 z-0'>
-                  {/* <Input 
-                    type='date'
-                    width={'120px'}
-                    defaultValue={12/12/2023}
-                    onTimeUpdate={true}
-                  /> */}
                   <DatePicker 
                   size='large'
                 />
@@ -156,6 +142,64 @@ function Landingpage() {
                 </div>
               ))}
             </div>
+          </div>
+        </div>
+        <div className={`z-50 absolute visible sm:invisible bg-white left-0 right-0 bottom-0 overflow-y-auto px-3`} style={{ top: `${yPosition}px` }}>
+          <div ref={ref} id='bar' className='absolute top-0  left-0 right-0 w-full cursor-move'>
+            <div className='w-full h-[2px] bg-gray-800 visible md:hidden'  />
+            <div className='w-full flex'>
+              <div className='m-auto w-[32px] h-2'>
+                <span className='w-[30px] bg-black h-[2px] py-[1px]'/>
+                <span className='w-[30px] bg-black h-[2px] py-[1px]'/>
+              </div>
+            </div>
+          </div>
+          <div className="w-full flex pr-3 justify-between visible sm:invisible h-auto lg:h-0 mt-6 relative">  
+            <div className='lg:flex'>
+              <div className='z-10 relative'>
+                <Dropdown />
+              </div>
+              <div className='w-[150px] mt-4 z-0'>
+                <DatePicker 
+                size='large'
+              />
+              </div>
+            </div>
+            <div className='flex min-w-[90px] float-right'>
+              <div className='my-auto'>
+                <div className='pl-2'>
+                  <Switch 
+                    color='green'
+                    defaultChecked
+                  />
+                </div>
+                <p className='text-black text-sm ml-2 text-right pt-[px]'>
+                  Save Search
+                </p>
+              </div>
+            </div>
+          </div>
+          <div className="w-full ">
+            {people.map((item, index) => (
+              <div key={index} className="shadow-xl p-4 sm:flex sm:h-40 rounded-xl border-[1px] my-2 mr-3">
+                <div className='w-full sm:w-auto justify-center content-center flex sm:static'>
+                  <img src={item.img} className="object-cover w-36 rounded-full sm:rounded-xl mb-5 sm:mb-auto sm:mx-auto"/>
+                </div>
+                <div className="px-6 overflow-hidden w-full">
+                  <div className="flex w-full">
+                    <p className="w-full text-black text-left text-sm md:text-xl">{"$"+item.price+"/month"}</p>
+                    <p className="text-right text-black text-sm md:text-xl zmdi zmdi-favorite-outline" />
+                  </div>
+                  <div className="w-full">
+                    
+                    <p className="w-full text-black text-left text-xs lg:text-lg">{item.name}</p>
+                    <p className="w-full pb-3 text-gray-500 text-sm md:text-md overflow-hidden">{truncateText(item.brief, 100)}</p>
+                    <div className="w-full justify-end h-[1px] bg-gray-500 my-1"/>
+                    <p className="w-full text-gray-500 text-left text-xs md:text-md">{item.title1+" | "+item.title12+" | "+" 9791 sqft"}</p>            
+                  </div>   
+                </div>
+              </div>
+            ))}
           </div>
         </div>
       </div>
