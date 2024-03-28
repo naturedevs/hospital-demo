91e33bb07b1f825389a95fe03d5e5ee0884d86a2
Jay <wmeng0103@gmail.com>
2024-03-28 13:49:33 +0900

    test

--- src/pages/landingpage/Landingpage.js
+++ src/pages/landingpage/Landingpage.js
@@ -59,7 +59,7 @@ function Landingpage() {
       if(rect.top > yPosition-18){
         const newY = e.touches[0].clientY - startDivY;
         console.log(newY)
-        if(newY > 18 && newY < screen.height-260) 
+        if(newY > 18 && newY < screen.height-100) 
         setYPosition(newY);
       }
       // setStartDivY(e.touches[0].clientY - yPosition)
